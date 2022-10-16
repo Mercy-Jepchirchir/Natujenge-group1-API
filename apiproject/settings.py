adeaca88b4fd9452f756c0309a99457478456422
Paul Ndambo <52633424+Paulndambo@users.noreply.github.com>
2022-10-16 20:28:40 +0300

    month added

--- apiproject/settings.py
+++ apiproject/settings.py
@@ -67,7 +67,7 @@ WSGI_APPLICATION = 'apiproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -86,6 +86,7 @@ DATABASES = {
         'PORT': 5432,
     }
 }
+"""
 
 
 # Password validation
--- apis/models.py
+++ apis/models.py
@@ -1,9 +1,24 @@
 from django.db import models
 
 # Create your models here.
+MONTH_CHOICES = (
+    ("January", "January"),
+    ("February", "February"),
+    ("March", "March"),
+    ("April", "April"),
+    ("May", "May"),
+    ("June", "June"),
+    ("July", "July"),
+    ("August", "August"),
+    ("September", "September"),
+    ("October", "October"),
+    ("November", "November"),
+    ("December", "December"),
+)
 class Bill(models.Model):
     title = models.CharField(max_length=255)
     amount = models.FloatField(default=0)
+    month = models.CharField(max_length=200, choices=MONTH_CHOICES, null=True, blank=True)
 
     def __str__(self):
         return self.title
